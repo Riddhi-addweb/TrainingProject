d3045e28f11e2b073bc13593720b397e0a2ae4c5
khushi-addweb <khushi@addwebsolution.in>
2023-05-23 17:17:44 +0530

    Whole Project Commit

--- src/test/java/Steps/Contract.java
+++ src/test/java/Steps/Contract.java
@@ -8,8 +8,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
